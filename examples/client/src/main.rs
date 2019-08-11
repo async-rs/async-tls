caa346c39154a7ca006acee892dcbebdaf5a1a22
Florian Gilcher <florian.gilcher@asquera.de>
2019-08-11 21:50:42 +0200

    Write comments in the client example

--- examples/client/src/main.rs
+++ examples/client/src/main.rs
@@ -26,29 +26,47 @@ struct Options {
 fn main() -> io::Result<()> {
     let options = Options::from_args();
 
+    // Check if the provided host exists
+    // TODO: this is blocking
     let addr = (options.host.as_str(), options.port)
         .to_socket_addrs()?
         .next()
         .ok_or_else(|| io::Error::from(io::ErrorKind::NotFound))?;
 
+    // If no domain was passed, the host is also the domain to connect to
     let domain_option = options.domain.unwrap_or(options.host);
+    // Use webpki::DNSNameRef to validate the domain for correctness
     let domain = DNSNameRef::try_from_ascii_str(&domain_option)
             .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid dnsname"))?
             .to_owned();
 
+    // Create a bare bones HTTP GET request
     let http_request = format!("GET / HTTP/1.0\r\nHost: {}\r\n\r\n", domain_option);
 
+    // Create default connector comes preconfigured with all you need to safely connect
+    // to remote servers!
     let connector = TlsConnector::default();
 
     task::block_on(async {
-        let stream = TcpStream::connect(&addr).await?;
+        // Open a normal TCP connection, just as you are used to
+        let tcp_stream = TcpStream::connect(&addr).await?;
 
-        let mut stream = connector.connect(&domain, stream)?.await?;
-        stream.write_all(http_request.as_bytes()).await?;
+        // Use the connector to start the handshake process.
+        // This might fail early if you pass an invalid domain
+        // (Which we already checked)
+        // This consumes the TCP stream.
+        let handshake = connector.connect(&domain, tcp_stream)?;
+        // Awaiting the handshake gives you an encrypted
+        // stream back which you can use like any other
+        let tls_stream = handshake.await?;
+        // We write our crafted HTTP request to it
+        tls_stream.write_all(http_request.as_bytes()).await?;
 
+        // And dump the whole thing to stdio
         let mut stdout = io::stdout();
         io::copy(&mut stream, &mut stdout).await?;
 
+        // Voila, we're done here!
         Ok(())
     })
 }
